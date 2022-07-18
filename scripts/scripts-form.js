09df0801106f776d396a854d5d842c16b3b9f65b
VERA <viraxyz@ukr.net>
2022-07-18 14:56:04 +0300

    fix prefered color theme

--- scripts/scripts-form.js
+++ scripts/scripts-form.js
@@ -1,16 +1,19 @@
 function changeImages(){
     let logo = document.getElementById('logo');
     let note = document.getElementById('note-icon');
-    let search = document.getElementById('search-icon');    
+    let search = document.getElementById('search-icon');
+    let vector = document.getElementById('vector');  
     let changer = document.getElementById('changer');
     if (changer.className === 'dark-theme'){
       logo.src = 'images/logo-dark.png';
       note.src = 'images/note-dark.png';
-      search.src = 'images/search-dark.png'
+      search.src = 'images/search-dark.png';
+      vector.src = 'images/vector-dark.png';
     } else {
         logo.src = 'images/logo-light.png';
         note.src = 'images/note-light.png';
         search.src = 'images/search-light.png';
+        vector.src = 'images/vector-light.png';
     }
   }
 
